40b53fa6fc43c09c907cbfa9997f7a50d3151d08
fariq <149790664+0xfrq@users.noreply.github.com>
2024-12-13 07:54:51 +0000

    tes codespace

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p;
+    cout << "Halo ";
+    cout << "Hai disana";
+    cin >> p;
+    cout << p;
+//disini aneh sekali
+//iya kenapa ya
+
+}
