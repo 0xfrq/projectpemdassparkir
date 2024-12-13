2ccd8d857017bb26d1abda98776432305b0dc7e5
BilmaVI <nabilmaulana2024@gmail.com>
2024-12-13 17:07:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,31 @@
 using namespace std;
 
 int main() {
-    int p;
-    cout << "Halo ";
-    cout << "Hai disana";
-    cin >> p;
-    cout << p;
-//disini aneh sekali
-//iya kenapa ya
-    cout <<"Github";
-    cout <<"Vscode";
+   int p1;
+   cout << endl << "=== Sistem Pengelolaan Parkir ===" << endl;
+   cout << "1. Kendaraan Masuk" << endl;
+   cout << "2. Kendaraan keluar" << endl;
+   cout << "3. Lihat Slot Parkir" << endl;
+   cout << "4. Exit Program" << endl;
+   cout << "Pilih Opsi: ";
+   cin >> p1;
+
+    switch(p1){
+        case 1:
+            cout << "."<< endl;
+            break;
+        case 2:
+            cout << ".."<< endl;
+            break;
+        case 3:
+            cout << "..."<< endl;
+            break;
+        case 4:
+            cout << "...."<< endl;
+            break;
+        default:
+            cout << "Pilihan tidak valid" << endl;
+            break;
+    }
 return 0;
 }
